01cbf2292b43676611811d37de64e74cbbe98160
Hanzhi <hanzhi@riema.xyz>
2025-07-14 11:20:44 -0700

    Add initial implementation of Nubit contract with ERC20, Ownable, and UUPS upgradeable features; include necessary OpenZeppelin contracts and settings

---
+++ contracts/NB.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract Nubit is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) initializer public {
+        __ERC20_init("Nubit", "NB");
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
